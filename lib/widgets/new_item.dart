42c503678fbdea9e36520c274461bdd9bf5a8fe5
trykea <tikearith@gmail.com>
2024-12-06 14:47:22 +0700

    added validators, button and also trigger the validator with Globalkey

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -10,59 +10,96 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
+  final _formkey = GlobalKey<FormState>();
+
+  void _saveItem(){
+    _formkey.currentState!.validate();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text('Add new Item'),
+        title: const Text('Add new Item'),
       ),
       body: Padding(
-        padding:const EdgeInsets.all(14),
-        child: Form(child: Column(
+        padding: const EdgeInsets.all(14),
+        child: Form(
+          key: _formkey,
+            child: Column(
           children: [
-              TextFormField(
-                maxLength: 50,
-                decoration:const InputDecoration(
-                  label: Text('Name'),
-                ),
-                validator: (value) {
-                  return 'err';
-                },
+            TextFormField(
+              maxLength: 50,
+              decoration: const InputDecoration(
+                label: Text('Name'),
+              ),
+              validator: (value) {
+                if (value == null ||
+                    value.isEmpty ||
+                    value.trim().length <= 1 ||
+                    value.trim().length > 50) {
+                  return 'Must between 1 and 50 characters';
+                }
+                return null;
+              },
             ),
             Row(
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Expanded(
                   child: TextFormField(
-                    decoration:const InputDecoration(
+                    decoration: const InputDecoration(
                       label: Text('Quantity'),
                     ),
                     initialValue: '1',
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value == null ||
+                          value.isEmpty ||
+                          int.tryParse(value)== null ||
+                          int.tryParse(value)! <= 0) {
+                        return 'Must be a valid, positive number';
+                      }
+                      return null;
+                    },
                   ),
                 ),
-                SizedBox(width: 24,),
+                const SizedBox(
+                  width: 24,
+                ),
                 Expanded(
                   child: DropdownButtonFormField(items: [
                     for (final category in categories.entries)
                       DropdownMenuItem(
                           value: category.value,
                           child: Row(
-                        children: [
-                          Container(
-                            width: 16,
-                            height: 16,
-                            color: category.value.color,
-                          ),
-                          const SizedBox(
-                            width: 6,
-                          ),
-                          Text(category.value.title),
-                        ],
-                      ))
+                            children: [
+                              Container(
+                                width: 16,
+                                height: 16,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(
+                                width: 6,
+                              ),
+                              Text(category.value.title),
+                            ],
+                          ))
                   ], onChanged: (value) {}),
                 )
               ],
             ),
+            const SizedBox(
+              height: 12,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(onPressed: () {
+                  _formkey.currentState!.reset();
+                }, child: const Text('Reset')),
+                ElevatedButton(onPressed: _saveItem, child: const Text('Add Item')),
+              ],
+            )
           ],
         )),
       ),
