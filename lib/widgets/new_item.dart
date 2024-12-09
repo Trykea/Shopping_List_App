7f89aa87921a55a66bcd027f414f98eace43d0f8
trykea <tikearith@gmail.com>
2024-12-09 14:49:52 +0700

    added flutter http and store added item in database instead of memory

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/category.dart';
 import 'package:shopping_list_app/models/grocery_item.dart';
@@ -19,11 +23,16 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formkey.currentState!.validate()) {
       _formkey.currentState!.save();
-      Navigator.of(context).pop(GroceryItem(
-          id: DateTime.now().toString(),
-          name: _enteredName,
-          quantity: _enteredQuantity,
-          category: _selectedCategory!));
+      final url = Uri.https('flutter-prep-f2f33-default-rtdb.asia-southeast1.firebasedatabase.app', 'shopping-list.json');
+      http.post(url, headers: {
+        'Content-Type' : 'application/json'
+      }, body:
+        json.encode({
+          'name': _enteredName,
+          'quantity': _enteredQuantity,
+          'category': _selectedCategory!.title,
+        }), );
+      // Navigator.of(context).pop();
     }
   }
 
